107fdd1b4fb2e2fd21cd5a942f0cbc54a383cfaa
dre1597 <74803456+dre1597@users.noreply.github.com>
2021-04-27 20:56:28 -0300

    icons and spaces

--- MangaList/controller/list-controller.js
+++ MangaList/controller/list-controller.js
@@ -8,20 +8,31 @@ export const createNewLine = (mangaName, status, en, ptbr, id) => {
     const content = `
         <td class="main__table--data manga-name">${mangaName}</td>
         <td class="main__table--data">${status}</td>
-        <td class="main__table--data">${en}</td>
-        <td class="main__table--data">${ptbr}</td>
         <td class="main__table--data">
-            <ul>
-                <li><a href="../views/update.html?id=${id}"><i class="fas fa-edit"></i></a></li>
-            </ul>
+            <div class="main__table--caps">
+                ${en}
+                <div class="main__table--buttons">
+                    <button type="button"><i class="fas fa-minus"></i></button>
+                    <button type="button"><i class="fas fa-plus"></i></button>
+                </div>
+            </div>
         </td>
         <td class="main__table--data">
-            <ul>
+            <div class="main__table--caps">
+                ${ptbr}
+                <div class="main__table--buttons">
+                    <button type="button"><i class="fas fa-minus"></i></button>
+                    <button type="button"><i class="fas fa-plus"></i></button>
+                </div>
+            </div>
+        </td>
+        <td class="main__table--data">
+            <ul class="main__table--options">
+                <li><a href="../views/update.html?id=${id}"><i class="fas fa-edit"></i></a></li>
                 <li><button class="button__delete" type="button" data-delete><i class="fas fa-trash"></i></button></li>
             </ul>
         </td>
     `
-    
     newLine.innerHTML = content
     newLine.dataset.id = id
     return newLine
